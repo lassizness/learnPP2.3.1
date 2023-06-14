2190052c3a25859ee1a4b7b32696c6ec09119b2f
lassizness <lassizness@gmail.com>
2023-06-14 16:58:42 +0500

    release 1.1

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,31 +1,26 @@
 package web.dao;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import web.entity.UserEntity;
 import web.model.User;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+@Repository
 @Transactional
 public class UserDaoImpl implements UserDao {
-    private final SessionFactory sessionFactory;
 
-    @Autowired
-    public UserDaoImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
     public List<User> getUsers() {
         List<User> users = new ArrayList<>();
-        Session session = sessionFactory.getCurrentSession();
-        List<UserEntity> usersEntity = session.createQuery("from UserEntity", UserEntity.class).list();
+        List<UserEntity> usersEntity = entityManager.createQuery("from UserEntity", UserEntity.class).getResultList();
         for (UserEntity userEntity : usersEntity) {
             users.add(new User(userEntity.getId(), userEntity.getName(), userEntity.getAge()));
         }
@@ -34,8 +29,7 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public User getUser(long id) {
-        Session session = sessionFactory.getCurrentSession();
-        UserEntity userEntity = session.get(UserEntity.class, id);
+        UserEntity userEntity = entityManager.find(UserEntity.class, id);
         User user = null;
         if (userEntity != null) {
             user = new User(userEntity.getId(), userEntity.getName(), userEntity.getAge());
@@ -45,29 +39,25 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public void saveUser(User user) {
-        Session session = sessionFactory.getCurrentSession();
         UserEntity userEntity = new UserEntity(user.getName(), user.getAge());
-        session.save(userEntity);
+        entityManager.persist(userEntity);
     }
 
     @Override
     public void updateUser(User user) {
-        Session session = sessionFactory.getCurrentSession();
-        UserEntity userEntity = session.get(UserEntity.class, user.getId());
-        System.out.println("updateUser"+user.getId());
+        UserEntity userEntity = entityManager.find(UserEntity.class, user.getId());
         if (userEntity != null) {
             userEntity.setName(user.getName());
             userEntity.setAge(user.getAge());
-            session.saveOrUpdate(userEntity);
+            entityManager.merge(userEntity);
         }
     }
 
     @Override
     public void deleteUser(long id) {
-        Session session = sessionFactory.getCurrentSession();
-        UserEntity userEntity = session.get(UserEntity.class, id);
+        UserEntity userEntity = entityManager.find(UserEntity.class, id);
         if (userEntity != null) {
-            session.delete(userEntity);
+            entityManager.remove(userEntity);
         }
     }
 }
